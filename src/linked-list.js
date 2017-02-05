0a7a356ad8f2fcf99374b480fe13974cf50d8713
Yuri Staravoitau <MorfinAll@mail.ru>
2017-02-05 18:25:30 +0300

    added DeleteAt(index) 8 tests passed

--- src/linked-list.js
+++ src/linked-list.js
@@ -58,7 +58,53 @@ class LinkedList {
 
     clear() {}
 
-    deleteAt(index) {}
+    deleteAt(index) {
+        var currentNode = this._head,
+        length = this.length,
+        count = 1,
+        beforeNodeToDelete = null,
+        nodeToDelete = null,
+        afterNodeToDelete=null,
+        deletedNode = null;
+        // not correct index
+        if (length == 0 || index < 1 || index > length) {
+            throw new Error('not correct index');
+        }
+        // for 1 index
+        if (index == 1) {
+            this._head = currentNode.next;
+            // next node exist
+            if (!this._head) {
+                this.head.prev = null;
+                // next node not exist
+            } 
+            else {
+                this._tail = null;
+            }
+
+
+        } // deleting last node
+        else if (index == this.length) {
+            this._tail = this._tail.prev;
+            this._tail.next = null;
+            // deleting middle node
+        } else {
+            while (count < index) {
+                currentNode = currentNode.next;
+                count++;
+            }
+
+            beforeNodeToDelete = currentNode.prev;
+            nodeToDelete = currentNode;
+            afterNodeToDelete = currentNode.next;
+
+            beforeNodeToDelete.next = afterNodeToDelete;
+            afterNodeToDelete.prev = beforeNodeToDelete;
+            deletedNode = nodeToDelete;
+            nodeToDelete = null;
+        }
+        this._length--;
+    }
 
     reverse() {}
 
