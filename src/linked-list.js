08f1195b05354719553299ac78956887751a41c9
Yuri Staravoitau <MorfinAll@mail.ru>
2017-02-05 17:31:32 +0300

    Updated append(data)

--- src/linked-list.js
+++ src/linked-list.js
@@ -8,12 +8,15 @@ class LinkedList {
     }
 
     append(data) {
+        var node = new Node(data);
         if (this.length ==0){
-            this._head = new Node(data,null,null);
-            this._tail = this._head;
+            this._head = node;
+            this._tail = node;
         }
         else {
-            this._tail = new Node (data,this._tail,null);
+            this._tail.next = node;
+            node.prev=this._tail;
+            node._tail=node;
         }
         this.length++;
         return this;
@@ -28,7 +31,22 @@ class LinkedList {
     }
 
     at(index) {
+        var currentNode = this._head,
+        length = this._length,
+        count = 1,
+        message = {failure: 'Failure: non-existent node in this list.'};
 
+        if (length === 0 || index < 1 || index > length) {
+            throw new Error(message.failure);
+        }
+
+        while (count < index) {
+            currentNode = currentNode.next;
+            count++;
+        }
+
+        //console.log(currentNode.data);
+        return currentNode.data;
     }
 
     insertAt(index, data) {
