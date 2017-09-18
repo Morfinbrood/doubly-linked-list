67383d1b4103142069f6a59a7decdfc9479d0f0a
Yury Staravoitau <MorfinAll@mail.ru>
2017-09-18 14:08:50 +0300

    Doubly-linked-list/refactoring: changed all var to let and deleting all unused brackets'{}'. The same test passing

--- src/linked-list.js
+++ src/linked-list.js
@@ -8,7 +8,7 @@ class LinkedList {
   }
 
   append(data) {
-    var node = new Node(data);
+    let node = new Node(data);
     if (this.length == 0) {
       this._head = node;
       this._tail = node;
@@ -30,8 +30,8 @@ class LinkedList {
   }
 
   at(index) {
-    var currentNode = this._head,
-      count = 0;
+    let currentNode = this._head;
+    let count = 0;
 
     while (count < index) {
       currentNode = currentNode.next;
@@ -41,14 +41,14 @@ class LinkedList {
   }
 
   insertAt(index, data) {
-    var currentNode = this._head,
-      count = 1;
+    let currentNode = this._head;
+    let count = 1;
 
     while (count < index) {
       currentNode = currentNode.next;
       count++;
     };
-    var node = new Node(data);
+    let node = new Node(data);
     node.prev = currentNode;
     node.next = currentNode.next;
     currentNode.next = node;
@@ -57,17 +57,15 @@ class LinkedList {
   }
 
   isEmpty() {
-    if (this.length == 0) {
+    if (this.length == 0)
       return true;
-    } else {
+    else
       return false;
-    }
   }
 
   clear() {
-    while (this.length > 1) {
+    while (this.length > 1)
       this.deleteAt(this.length);
-    };
     this._head.data = null;
     this._tail.data = null;
     this.length--;
@@ -75,25 +73,25 @@ class LinkedList {
   }
 
   deleteAt(index) {
-    var currentNode = this._head,
-      length = this.length,
-      count = 1,
-      prevNode = null,
-      nodeToDelete = null,
-      nextNode = null,
-      deletedNode = null;
+    let currentNode = this._head;
+    let length = this.length;
+    let count = 1;
+    let prevNode = null;
+    let nodeToDelete = null;
+    let nextNode = null;
+    let deletedNode = null;
     // for 0 index
     if (index == 0) {
       this._head = currentNode.next;
       // next node exist
-      if (this._head != null) {
+      if (this._head != null)
         this.head.prev = null;
-        // next node not exist
-      } else {
+      // next node not exist
+      else
         this._tail = null;
-      }
     } // deleting last node
-    else if (index == this.length) {
+    else
+    if (index == this.length) {
       this._tail = this._tail.prev;
       this._tail.next = null;
       // deleting middle node
@@ -102,28 +100,27 @@ class LinkedList {
         currentNode = currentNode.next;
         count++;
       }
-
       prevNode = currentNode.prev;
       nodeToDelete = currentNode;
       nextNode = currentNode.next;
-
       prevNode.next = nextNode;
       nextNode.prev = prevNode;
       deletedNode = nodeToDelete;
       nodeToDelete = null;
     }
+
     this.length--;
     return this;
   }
 
   reverse() {
-    var currentNode = this._head,
-      length = this.length,
-      count = 1,
-      arrayNewList = [];
+    let currentNode = this._head;
+    let length = this.length;
+    let count = 1;
+    let arrayNewList = [];
 
-    var oldHead = this._head,
-      oldTail = this._tail;
+    let oldHead = this._head;
+    let oldTail = this._tail;
 
     while (count < this.length - 1) {
       currentNode = currentNode.next;
@@ -132,9 +129,9 @@ class LinkedList {
     };
 
     arrayNewList.reverse();
-    var count = 1,
-      i = 0,
-      currentNode = this._head;
+    count = 1;
+    let i = 0;
+    currentNode = this._head;
     while (count < this.length - 1) {
       currentNode = currentNode.next;
       currentNode.data = arrayNewList[i];
@@ -142,22 +139,21 @@ class LinkedList {
       count++;
     };
 
-    var oldHeadData = this._head.data,
-      oldTailData = this._tail.data;
+    let oldHeadData = this._head.data;
+    let oldTailData = this._tail.data;
     this._head.data = oldTailData;
     this._tail.data = oldHeadData;
     return this;
   }
 
   indexOf(data) {
-    var currentNode = this._head,
-      count = 0,
-      index = -1;
+    let currentNode = this._head;
+    let count = 0;
+    let index = -1;
 
     while (count < this.length) {
-      if (currentNode.data == data) {
+      if (currentNode.data == data)
         index = count;
-      }
       currentNode = currentNode.next;
       count++;
     };
